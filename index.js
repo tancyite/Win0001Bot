0c6d837e0cf40467904e93d99aa2cf31b439bd35
tancyite <44539484+tancyite@users.noreply.github.com>
2018-10-28 08:16:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js');
 const Winbot = new Discord.Client();
 
-    // вход
-Winbot.login(process.env.BOT_TOKEN);
-
-
 Winbot.on('ready', () => {
     console.log('Готов работать!');
 });
@@ -14,3 +10,6 @@ if(message.content == "vk" ) {
     message.reply("Группа: https://vk.com/streamwin");
 }
 });
+
+    // вход
+Winbot.login(process.env.BOT_TOKEN);
